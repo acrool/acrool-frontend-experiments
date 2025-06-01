10e0b568dca9c8e0a47d2ebd11a2aa5e364b7713
imagine10255 <imagine10255@gmail.com>
2025-06-01 15:14:34 +0800

    Refactor component file structure and simplify GitHub Actions workflow

--- src/components/organize/AutoOverflowAndFlexHeight/AutoOverflowAndFlexHeight.stories.tsx
+++ src/components/organize/AutoOverflowAndFlexHeight/AutoOverflowAndFlexHeight.stories.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import {html} from "./data";
 
 const meta = {
-    title: 'Laboratory/AutoOverflowAndFlexHeight',
+    title: 'Organize/AutoOverflowAndFlexHeight',
     component: AutoOverflowAndFlexHeight,
     parameters: {
         docs: {
