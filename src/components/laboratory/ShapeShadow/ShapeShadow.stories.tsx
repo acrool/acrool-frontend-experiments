22161a86d7e974f6a31eeeef77ce06e245bb4c50
imagine <imagine10255@gmail.com>
2025-08-18 15:48:34 +0800

    測試 svg 文字外框

--- src/components/laboratory/ShapeShadow/ShapeShadow.stories.tsx
+++ src/components/laboratory/ShapeShadow/ShapeShadow.stories.tsx
@@ -1,6 +1,7 @@
 import {Flex} from '@acrool/react-grid';
 import type {Meta, StoryObj} from '@storybook/react-vite';
 import React from 'react';
+import styled from "styled-components";
 
 
 const meta = {
@@ -25,7 +26,7 @@ type Story = StoryObj<typeof meta>;
 
 
 
-export const WithFilterDropShadow: Story = {
+export const ImageWithFilterDropShadow: Story = {
     args: {},
     render: function Render(args) {
         return <img src="/static/acrool_logo.svg"
@@ -38,7 +39,7 @@ export const WithFilterDropShadow: Story = {
     }
 };
 
-export const WithBoxShadow: Story = {
+export const ImageWithBoxShadow: Story = {
     args: {},
     render: function Render(args) {
         return <img src="/static/acrool_logo.svg"
@@ -50,3 +51,58 @@ export const WithBoxShadow: Story = {
             }}/>;
     }
 };
+
+
+export const TextWithBoxShadow: Story = {
+    args: {},
+    render: function Render(args) {
+        return <Flex column className="gap-4">
+            <p
+                style={{
+                    fontSize: '20px',
+                    filter: 'drop-shadow(-13px 5px 10px red)'
+                }}>
+                Test Text with Box Shadow
+            </p>
+            <p
+                style={{
+                    fontSize: '20px',
+                    filter: 'drop-shadow(1px 0 0 red) drop-shadow(-1px 0 0 red) drop-shadow(0 1px 0 red) drop-shadow(0 -1px 0 red)'
+                }}>
+                Test Text with Box Shadow
+            </p>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="800" height="200">
+                <CustomText
+                    className="text stroke"
+                    alignmentBaseline="text-before-edge"
+                    textAnchor="start"
+                >
+                    這是一段很長的文字，會自動換行， 因為這其實是 HTML 的。
+                </CustomText>
+            </svg>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="800" height="200">
+                <foreignObject width="100%" height="100%">
+                            這是一段很長的文字，會自動換行，
+                            因為這其實是 HTML 的。
+                </foreignObject>
+            </svg>
+        </Flex>;
+    }
+}
+
+
+
+
+const CustomText = styled.text`
+    font-size: 50px;
+    color: red;
+    fill: #fff;
+    font-weight: 400;
+
+    stroke: #000;
+    stroke-width: 4px;
+    paint-order: stroke;
+`;
+
